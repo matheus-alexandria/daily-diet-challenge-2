9009d8b97bd5bbcd4b84c4ef432080770c1fcc2a
Matchobas <maalexandria@hotmail.com>
2023-07-14 22:14:59 -0300

    Add: prisma first database schema migrations

---
+++ prisma/migrations/20230715011418_create_user_and_meal/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "meals" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "schedule" TIMESTAMP(3) NOT NULL,
+    "on_diet" BOOLEAN NOT NULL,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "meals_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "meals" ADD CONSTRAINT "meals_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
