714ecf81e1dabc6469584ef8359415790842184f
Matchobas <maalexandria@hotmail.com>
2023-07-15 21:43:52 -0300

    Chore: create user route with session id

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,14 +1,21 @@
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
+import { randomUUID } from "crypto";
 
-export function appRoutes(app: FastifyInstance) {
+export async function appRoutes(app: FastifyInstance) {
   app.post('/user', async (request, reply) => {
     const requestBodySchema = z.object({
       name: z.string(),
     });
 
     const { name } = requestBodySchema.parse(request.body);
+
+    const cookie = randomUUID();
+    reply.cookie('sessionId', cookie, {
+      path: '/',
+      maxAge: 1000 * 60 * 60 * 24 * 7 // A week (7 days)
+    });
 
     await prisma.user.create({
       data: {
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import fastify from "fastify";
+import cookie from '@fastify/cookie';
 import { env } from "./env";
+import { appRoutes } from "./http/routes";
 
 const app = fastify();
+
+app.register(cookie);
+
+app.register(appRoutes);
 
 app.listen({
   port: env.PORT,
   host: '0.0.0.0',
 }, () => {
-  console.log('Server Daily Diet Online')
+  console.log(`Daily Diet Online on ${env.PORT}`)
 });
